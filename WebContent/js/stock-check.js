b72afcbdd6ae3baf33c501106b2cb10a6357fbc1
user001 <sugahara_a@worksap.co.jp>
2017-01-17 18:28:23 +0900

    save

--- WebContent/js/stock-check.js
+++ WebContent/js/stock-check.js
@@ -1,43 +1,47 @@
-// ID取得
-var id = parseInt($.url().param('id'));
-$("input[name=id]").val(id);
 
-// 商品情報読み込み
-var sql = 'SELECT * \
-	FROM stock \
-	JOIN whouse ON whouse.id = stock.whouse \
-	JOIN item ON item.id = stock.item \
-	JOIN kind ON kind.id = item.kind \
-	WHERE stock.id = ?';
-var row = alasql(sql, [ id ])[0];
-$('#image').attr('src', 'img/' + row.item.id + '.jpg');
-$('#whouse').text(row.whouse.name);
-$('#code').text(row.item.code);
-$('#maker').text(row.item.maker);
-$('#detail').text(row.item.detail);
-$('#price').text(numberWithCommas(row.item.price));
-var balance = row.stock.balance; // 入出庫で利用
-$('#balance').text(balance);
+//標準偏差
+$(function(){
 
-// トランザクション読み込み
-var rows = alasql('SELECT * FROM trans WHERE stock = ?', [ id ]);
-var tbody = $('#tbody-transs');
-for (var i = 0; i < rows.length; i++) {
-	var row = rows[i];
-	var tr = $('<tr>').appendTo(tbody);
-	tr.append('<td>' + row.trans.date + '</td>');
-	tr.append('<td>' + row.trans.qty + '</td>');
-	tr.append('<td>' + row.trans.balance + '</td>');
-	tr.append('<td>' + row.trans.memo + '</td>');
+//標準偏差：平均
+var hensa_box = [32,27,29,34,33];
+
+function getAverage(hensa_box){
+	var sum = 0;
+	var average = -1;
+	for(var i=0; i<hensa_box.length; i++){
+		sum += hensa_box[i];
+	}
+	average = sum / hensa_box.length;
+	return average;
 }
 
-// 入庫・出庫処理
-$('#update').on('click', function() {
-	var date = $('input[name="date"]').val();
-	var qty = parseInt($('input[name="qty"]').val());
-	var memo = $('textarea[name="memo"]').val();
-	alasql('UPDATE stock SET balance = ? WHERE id = ?', [ balance + qty, id ]);
-	var trans_id = alasql('SELECT MAX(id) + 1 as id FROM trans')[0].id;
-	alasql('INSERT INTO trans VALUES(?,?,?,?,?,?)', [ trans_id, id, date, qty, balance + qty, memo ]);
-	window.location.assign('stock.html?id=' + id);
-});
+//標準偏差：分散
+function getVariance(hensa_box){
+	var average = getAverage(hensa_box);
+	var variance = -1;
+	var sum = 0;
+	for (var i=0; i<hensa_box.length; i++){
+		sum += Math.pow((hensa_box[i] - average), 2);
+	}
+	variance = sum / hensa_box.length;
+	return variance;
+}
+
+//標準偏差(分散のルート)
+function getStandardDeviation(hensa_box){
+	var variance = getVariance(hensa_box);
+	var standardDeviation = Math.sqrt(variance);
+	return standardDeviation;
+}
+
+var hensa = getStandardDeviation(hensa_box);
+//四捨五入
+function floatFormat(number){
+	var _pow = Math.pow(10, 2);
+	return Math.round(number * _pow) / _pow;
+}
+var hensa_float = floatFormat(hensa);
+
+$("#hensa").append(hensa);
+$("#hensa").append(hensa_float);
+})
