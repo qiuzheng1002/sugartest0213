31407025c12c35e32f5f496a05afad958c0f5364
user001 <sugahara_a@worksap.co.jp>
2017-01-24 16:37:26 +0900

    save

--- WebContent/js/stock-out.js
+++ WebContent/js/stock-out.js
@@ -16,13 +16,13 @@ for (var i = 0; i < rows.length; i++) {
 		tr.append('<td>' + numberWithCommas(row.trans.num) + '</td>');	
 		tr.append('<td>' + row.trans.deadline + '</td>');
 		if (state_check == 4){
-			tr.append('<td>' + '<span class="label label-danger">受注済み</span>' + '</td>');
+			tr.append('<td>' + '<span class="label label-danger" style="font-size:86%;">受注済み</span>' + '</td>');
 		}
 		else if (state_check == 5){
-			tr.append('<td>' + '<span class="label label-warning">納期回答済み</span>' + '</td>');
+			tr.append('<td>' + '<span class="label label-warning" style="font-size:86%;">納期回答済み</span>' + '</td>');
 		}
 		else if (state_check == 6){
-			tr.append('<td>' + '<span class="label label-success">出庫済み</span>' + '</td>');
+			tr.append('<td>' + '<span class="label label-success" style="font-size:86%;">出庫済み</span>' + '</td>');
 		}
 	tr.append('<td class="text-right"><button type="button" class="btn btn-xs" id="delete_data_address" data-toggle="modal" data-target="#delete_data"><span class="glyphicon glyphicon-remove"></span></button></td>');
 	}
