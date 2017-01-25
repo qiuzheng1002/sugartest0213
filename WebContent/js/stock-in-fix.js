c4293f14d4c7969f736fb79a91975da694501799
user001 <sugahara_a@worksap.co.jp>
2017-01-25 17:59:30 +0900

    save

---
+++ WebContent/js/stock-in-fix.js
@@ -0,0 +1,27 @@
+// ID取得
+var id = parseInt($.url().param('id'));
+$("input[name=id]").val(id);
+
+//変更
+// トランザクション読み込み
+var rows = alasql('SELECT * FROM trans WHERE stock = ?', [ id ]);
+var tbody = $('#tbody-transs');
+for (var i = 0; i < rows.length; i++) {
+	var row = rows[i];
+	var tr = $('<tr>').appendTo(tbody);
+	tr.append('<td>' + row.trans.date + '</td>');
+	if (row.trans.order_wh === 0){
+		tr.append('<td style="color: #FFFFFF;">' + row.trans.order_wh + '</td>');}
+	else {
+		tr.append('<td>' + row.trans.order_wh + '</td>');}
+	if (row.trans.in_wh === 0){
+		tr.append('<td style="color: #FFFFFF;">' + row.trans.in_wh + '</td>');}
+	else {
+		tr.append('<td>' + row.trans.in_wh + '</td>');}
+	if (row.trans.out_wh === 0){
+		tr.append('<td style="color: #FFFFFF;">' + row.trans.out_wh + '</td>');}
+	else {
+		tr.append('<td>' + row.trans.out_wh + '</td>');}
+	tr.append('<td>' + row.trans.memo + '</td>');
+	tr.append('<td class="text-right"><button type="button" class="btn btn-xs" id="delete_data_address" data-toggle="modal" data-target="#delete_data"><span class="glyphicon glyphicon-remove"></span></button></td>');
+}
--- WebContent/js/stock-out.js
+++ WebContent/js/stock-out.js
@@ -16,13 +16,13 @@ for (var i = 0; i < rows.length; i++) {
 		tr.append('<td>' + numberWithCommas(row.trans.num) + '</td>');	
 		tr.append('<td>' + row.trans.deadline + '</td>');
 		if (state_check == 4){
-			tr.append('<td>' + '<button class="btn btn-danger btn-xs">受注済み</button>' + '</td>');
+			tr.append('<td>' + '<button class="btn btn-danger btn-xs" id="fix_data_address">受注済み</button>' + '</td>');
 		}
 		else if (state_check == 5){
-			tr.append('<td>' + '<button class="btn btn-warning btn-xs">納期確定済み</button>' + '</td>');
+			tr.append('<td>' + '<button class="btn btn-warning btn-xs" id="fix_data_address">納期確定済み</button>' + '</td>');
 		}
 		else if (state_check == 6){
-			tr.append('<td>' + '<button class="btn btn-success btn-xs">出庫済み</button>' + '</td>');
+			tr.append('<td>' + '<button class="btn btn-success btn-xs" id="fix_data_address">出庫済み</button>' + '</td>');
 		}
 	tr.append('<td class="text-right"><button type="button" class="btn btn-xs" id="delete_data_address" data-toggle="modal" data-target="#delete_data"><span class="glyphicon glyphicon-remove"></span></button></td>');
 	tr.append('<td></td>');
@@ -46,7 +46,6 @@ for (var i = 0; i < shop_rows.length; i++) {
 	var shop_row = shop_rows[i];
 	$('<option value = "' + shop_row.shop + '">').appendTo('#shops');
 }
-
 
 //受注データ登録ボタン
 $('#update_order').on('click', function() {
@@ -211,6 +210,76 @@ $(function(){
 	$("#selected_date1").attr("value", selected_date);
 	var selected_deadline = y + '-' + m + '-' + d + ' 00:00';
 	$("#selected_deadline1").attr("value", selected_deadline);
+});
+
+//履歴データ編集 
+$(function(){
+	$(document).on("click","#fix_data_address",function() {
+		var fix_data_address = $(this).parent().parent();
+		console.log(fix_data_address);
+		var tableid = document.getElementById('syukko_process_table');
+		var fd_address_rows = fix_data_address.index(this.rowIndex) + 1;
+		var fix_date = tableid.rows[fd_address_rows].cells[0].innerText;
+		var fix_shop = tableid.rows[fd_address_rows].cells[1].innerText;
+		var fix_num_comma = tableid.rows[fd_address_rows].cells[2].innerText;
+		var fix_num = parseInt(fix_num_comma.replace(/,/g,""));
+		var fix_deadline = tableid.rows[fd_address_rows].cells[3].innerText;
+		var fix_state_num = tableid.rows[fd_address_rows].cells[4].innerText;
+		if (fix_state_num == "出庫済み"){
+			var fix_state = 6;
+		}
+		else if (delete_state_num == "納期確定済み"){
+			var fix_state = 5;
+		}
+		else if (delete_state_num == "受注済み"){
+			var fix_state = 4;
+		}
+		var fix_words = "WHERE date = '" + fix_date + "' AND shop = '" + fix_shop + "' AND num = " + fix_num + " AND deadline = '" + fix_deadline + "' AND state = " + fix_state ;
+		var fix_row_id = alasql('SELECT id FROM trans ' + fix_words)[0]; //編集するデータのidを割り出す (条件だけで検索すると、同じ条件が複数あった場合にバグる)
+		fix_data_address = "";
+		window.location.assign('stock-out.html?id=' + fix_row_id);
+	});
+});
+
+
+
+
+
+
+
+
+//履歴データ削除
+var delete_data_address=""; 
+$(function(){ //削除箇所を先に指定 (モーダルダイアログでparentが使えない)
+	$(document).on("click","#delete_data_address",function() {
+		delete_data_address = $(this).parent().parent();
+	});
+});
+$(function(){ //SQL内のデータを削除し、結果的にテーブルから削除
+	$(document).on("click","#destroy_data",function() {
+		var tableid = document.getElementById('syukko_process_table');
+		var dd_address_rows = delete_data_address.index(this.rowIndex) + 1;
+		var delete_date = tableid.rows[dd_address_rows].cells[0].innerText;
+		var delete_shop = tableid.rows[dd_address_rows].cells[1].innerText;
+		var delete_num_comma = tableid.rows[dd_address_rows].cells[2].innerText;
+		var delete_num = parseInt(delete_num_comma.replace(/,/g,""));
+		var delete_deadline = tableid.rows[dd_address_rows].cells[3].innerText;
+		var delete_state_num = tableid.rows[dd_address_rows].cells[4].innerText;
+		if (delete_state_num == "出庫済み"){
+			var delete_state = 6;
+		}
+		else if (delete_state_num == "納期確定済み"){
+			var delete_state = 5;
+		}
+		else if (delete_state_num == "受注済み"){
+			var delete_state = 4;
+		}
+		var delete_words = "WHERE date = '" + delete_date + "' AND shop = '" + delete_shop + "' AND num = " + delete_num + " AND deadline = '" + delete_deadline + "' AND state = " + delete_state ;
+		var delete_row_id = alasql('SELECT id FROM trans ' + delete_words)[0]; //削除するデータのidを割り出す (条件だけで削除すると全く同じ条件の場合、該当案件全てが消える)
+		var delete_row = alasql('DELETE FROM trans WHERE id = ' + delete_row_id["id"])[0]; //idを用いてデータを削除する
+		delete_data_address = "";
+		window.location.assign('stock-out.html?id=' + id);
+	});
 });
 
 
