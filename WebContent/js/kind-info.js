f07358610e3e595c39e21829a8e00a0ea4447b30
user001 <sugahara_a@worksap.co.jp>
2017-02-03 18:27:31 +0900

    save

--- WebContent/js/kind-info.js
+++ WebContent/js/kind-info.js
@@ -1,59 +1,40 @@
-// 検索ボックス作成
-var rows = alasql('SELECT * FROM whouse;');
-for (var i = 0; i < rows.length; i++) {
-	var row = rows[i];
-	var option = $('<option>');
-	option.attr('value', row.whouse.id);
-	option.text(row.whouse.name);
-	$('select[name="q1"]').append(option);
+// ID取得
+var id = parseInt($.url().param('id'));
+$("input[name=id]").val(id);
+
+//データ事前入力
+if (isNaN(id) == true){
+	$("#this_bread").text("NewData"); //パンくずリスト
+	var kind_id = alasql('SELECT MAX(id) + 1 as id FROM kind')[0].id;
+	$("#kind_data_id_input").attr("value", kind_id); //data No.
+}
+else{
+	var kind_object = alasql('SELECT * FROM kind WHERE id = ?', [ id ])[0];
+	var kind = $(kind_object).attr("kind"); //object皮むき
+	$("#this_bread").text("Data No. " + kind.id + " - " + kind.text); //パンくずリスト
+	$("#kind_data_id_input").attr("value", kind.id); //data No.
+	$("#kind_data_text_input").attr("value", kind.text); //商品区分
 }
 
-var rows = alasql('SELECT * FROM kind;');
-for (var i = 0; i < rows.length; i++) {
-	var row = rows[i];
-	var option = $('<option>');
-	option.attr('value', row.kind.id);
-	option.text(row.kind.text);
-	$('select[name="q2"]').append(option);
-}
+//履歴データ編集
+$(function(){
+	$(document).on("click","#update_kind",function() {
+		if (isNaN(id) == true){
+			var kind_id = alasql('SELECT MAX(id) + 1 as id FROM kind')[0].id;
+			var kind_text = $('#kind_data_text_input').attr("value");
+			//既存のデータと重複チェック
+			alasql('INSERT INTO kind VALUES(?,?)', [ kind_id, kind_text ]);			
+		}
+		else{
+			var kind_id = id;
+			var kind_text = $('#kind_data_text_input').attr("value");
+			alasql("UPDATE kind SET text = '" + kind_text + "' WHERE id = " + kind_id);
+		}
+		window.location.assign('index-kind-info.html');
+	});
+});
 
-// 検索条件の取得
-var q1 = parseInt($.url().param('q1') || '0');
-$('select[name="q1"]').val(q1);
-var q2 = parseInt($.url().param('q2') || '0');
-$('select[name="q2"]').val(q2);
-var q3 = $.url().param('q3') || '';
-$('input[name="q3"]').val(q3);
-
-// SQLの生成
-var sql = 'SELECT * \
-	FROM stock \
-	JOIN whouse ON whouse.id = stock.whouse \
-	JOIN item ON item.id = stock.item \
-	JOIN kind ON kind.id = item.kind \
-	WHERE item.code LIKE ? ';
-
-sql += q1 ? 'AND whouse.id = ' + q1 + ' ' : '';
-sql += q2 ? 'AND kind.id = ' + q2 + ' ' : '';
-
-// SQL実行
-var stocks = alasql(sql, [ q3 + '%' ]);
-
-// HTML作成
-var tbody = $('#tbody-stocks');
-for (var i = 0; i < stocks.length; i++) {
-	var stock = stocks[i];
-	var tr = $('<tr data-href="item-info.html?id=' + stock.stock.id + '"></tr>');
-	tr.append('<td>' + stock.whouse.name + '</td>');
-	tr.append('<td>' + stock.kind.text + '</td>');
-	tr.append('<td>' + stock.item.code + '</td>');
-	tr.append('<td>' + stock.item.maker + '</td>');
-	tr.append('<td>' + stock.item.detail + '</td>');
-	tr.append('<td style="text-align: right;">' + numberWithCommas(stock.item.price) + '</td>');//未出庫数にする
-	tr.appendTo(tbody);
-}
-
-// クリック動作
-$('tbody > tr').css('cursor', 'pointer').on('click', function() {
-	window.location = $(this).attr('data-href');
+//ツールチップ
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
 });
